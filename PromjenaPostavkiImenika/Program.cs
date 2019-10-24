b37c434d343ec8aba6b286f91e1663971c86ea3e
Sven Gotal <svgotal@vsite.hr>
2019-10-24 20:14:25 +0200

    Add project with custom namespace

---
+++ PromjenaPostavkiImenika/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vsite.CSharp.StrukturePrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dobar dan");
+            Console.ReadKey();
+        }
+    }
+}
