5205fa074cb28c19eb7460a23f9ab0918cd82db3
Marko Vitković <mavitkov@vsite.hr>
2019-10-24 20:04:04 +0200

    resolve

--- DokumentacijskiKomentari/DokumentacijskiKomentari.cs
+++ DokumentacijskiKomentari/DokumentacijskiKomentari.cs
@@ -5,12 +5,16 @@ using System.Text;
 
 namespace Vsite.CSharp.StrukturaPrograma
 {
+	/// <summary>
+	/// Primjer dokumentacijkih komentara
+	/// </summary>
     class DokumentacijskiKomentari
     {
         static void Main(string[] args)
         {
             Console.WriteLine(Kvadrat(5));
             Console.WriteLine(DuljinaDvijeRiječi("dva", "2"));
+			Console.WriteLine(Math.Sqrt(2.0));
         }
 
         /// <summary>
@@ -27,7 +31,13 @@ namespace Vsite.CSharp.StrukturaPrograma
             return broj * broj;
         }
 
-        // TODO:010 Dodati dokumentacijski komentar za donju metodu i provjeriti pojavljuje li se opis u oblačiću na mjestu poziva metode u metodi Main.
+        
+		/// <summary>
+		/// Izracunava duljinu dvije rijeci
+		/// </summary>
+		/// <param name="prva">Prva rijec</param>
+		/// <param name="druga">Druga rijec</param>
+		/// <returns>Duljina dvije rijeci</returns>
         static int DuljinaDvijeRiječi(string prva, string druga)
         {
             return (prva + druga).Length;
