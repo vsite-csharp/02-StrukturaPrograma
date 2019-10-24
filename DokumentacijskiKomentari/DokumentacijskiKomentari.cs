190f1a6441010e7dcc456717a45fdc83dbbf548c
Domagoj Beronic <domagojberonic@outlook.com>
2019-10-24 20:03:56 +0200

    Resolve NameSpace

--- DokumentacijskiKomentari/DokumentacijskiKomentari.cs
+++ DokumentacijskiKomentari/DokumentacijskiKomentari.cs
@@ -5,12 +5,16 @@ using System.Text;
 
 namespace Vsite.CSharp.StrukturaPrograma
 {
+    /// <summary>
+    /// Primjer dokumentacijskih komentar
+    /// </summary>
     class DokumentacijskiKomentari
     {
         static void Main(string[] args)
         {
             Console.WriteLine(Kvadrat(5));
             Console.WriteLine(DuljinaDvijeRiječi("dva", "2"));
+            Console.WriteLine(Math.Sqrt(2.0));
         }
 
         /// <summary>
@@ -28,6 +32,12 @@ namespace Vsite.CSharp.StrukturaPrograma
         }
 
         // TODO:010 Dodati dokumentacijski komentar za donju metodu i provjeriti pojavljuje li se opis u oblačiću na mjestu poziva metode u metodi Main.
+        /// <summary>
+        /// Izracunava duljinu ukupnu duljinu dvije rijeci
+        /// </summary>
+        /// <param name="prva"> Prva rijec.</param>
+        /// <param name="druga">Druga rijec</param>
+        /// <returns>Cijeli broj jednk ukupnoj duljinu</returns>
         static int DuljinaDvijeRiječi(string prva, string druga)
         {
             return (prva + druga).Length;
