9200ae0100cc35b80493e62ca64d1566204bde78
Senad Mandžić <semandzi@vsite.hr>
2019-10-24 19:48:47 +0200

    Remove superflous

--- Main/PraviMain.cs
+++ Main/PraviMain.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 
-// TODO:000 Napraviti build projekta i pogledati koju pogrešku prevoditelj javlja. Usporediti metode Main u klasama PraviMain i JošJedanMain.
 
-// TODO:001 U postavkama projekta staviti da je klasa PraviMain ulazna (Startup object) te pokrenuti novi build.
 
 namespace Vsite.CSharp.StrukturaPrograma
 {
@@ -13,12 +11,12 @@ namespace Vsite.CSharp.StrukturaPrograma
             Console.WriteLine("Krećem iz PraviMain.Main(string[] args)");
         }
 
-        // TODO:002 Zakomentirati ili preimenovati donju metodu i napraviti build projekta te ga pokrenuti.
-        static void Main()
+        
+        /*static void Main()
         {
             Console.WriteLine("Krećem iz PraviMain.Main()");
         }
-
+        */
         public static void main()
         {
             Console.WriteLine("main()");
