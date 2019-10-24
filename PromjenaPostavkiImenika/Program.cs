f83417b71c61c0924265d5ff104d102d7642fc3d
Ivan Mihin <ivmihin@vsite.hr>
2019-10-24 20:14:01 +0200

    Added a project with a custom namespace

---
+++ PromjenaPostavkiImenika/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vsite.CSharp.StrukturaPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dobar dan.");
+            Console.ReadKey();
+        }
+    }
+}
