a346adf5e91f99aad67d7c5c2d390e9e7d965f41
Filip Velebit Samodol <fisamodo@vsite.hr>
2019-10-24 20:13:45 +0200

    add project with custom namespaces

---
+++ PromjenaImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Csharp.StrukturaPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dobar dan");
+            Console.ReadKey();
+        }
+    }
+}
