d6735ce7113d0d75e0bc5aa940eda0c5ad247496
Domagoj Beronic <domagojberonic@outlook.com>
2019-10-24 20:13:43 +0200

    Add project with custom namespace

---
+++ PromjenaPostavkiImenika/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vsite.CSharp.StrukturaPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dobar dan");
+            Console.ReadKey();
+        }
+    }
+}
