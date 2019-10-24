0a6b699f5933d229d0c0e6458035475df01a9fb2
db <darij.b@gmail.com>
2019-10-24 20:13:53 +0200

    Add project with namespace

---
+++ Promjena imenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.StrukturaPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dobar dan");
+            Console.ReadKey();
+        }
+    }
+}
