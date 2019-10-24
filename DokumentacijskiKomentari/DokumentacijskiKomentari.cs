31d8bc6ab8ad1664947c94df8c228642d998f002
Dona Pejnović <don@DESKTOP-QROSDFJ>
2019-10-24 19:58:06 +0200

    -Add documentation comments

--- DokumentacijskiKomentari/DokumentacijskiKomentari.cs
+++ DokumentacijskiKomentari/DokumentacijskiKomentari.cs
@@ -7,10 +7,15 @@ namespace Vsite.CSharp.StrukturaPrograma
 {
     class DokumentacijskiKomentari
     {
+        /// <summary>
+        /// Primjer dokumentacijskih komentara 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine(Kvadrat(5));
             Console.WriteLine(DuljinaDvijeRiječi("dva", "2"));
+            Console.WriteLine(Math.Sqrt(2.0));
         }
 
         /// <summary>
@@ -27,7 +32,13 @@ namespace Vsite.CSharp.StrukturaPrograma
             return broj * broj;
         }
 
-        // TODO:010 Dodati dokumentacijski komentar za donju metodu i provjeriti pojavljuje li se opis u oblačiću na mjestu poziva metode u metodi Main.
+
+        /// <summary>
+        /// Izračunava ukupnu duljinnu dvije riječi.
+        /// </summary>
+        /// <param name="prva">Prva riječ.</param>
+        /// <param name="druga">Druga riječ.</param>
+        /// <returns>Cijeli broj jednak ukupnoj duljini riječi.</returns>
         static int DuljinaDvijeRiječi(string prva, string druga)
         {
             return (prva + druga).Length;
