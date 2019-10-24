ec7b46e013d67d21aaeb837439d180f8b7ac863e
Krunoslav Bučar <krbuki@gmail.com>
2019-10-24 20:18:39 +0100

    Add project with custom namespace

---
+++ PromjenaPostavkiImenika/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Vsite.CSharp.StrukturePrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dobar dan");
+            Console.ReadKey();//čeka da pritisnemo tipku
+           
+        }
+    }
+}
