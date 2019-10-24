95cea08e21809e80794da45f05ec60254313940f
tleva <tleva@DESKTOP-J8PKM7S>
2019-10-24 20:04:10 +0200

    Gotovo

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -5,14 +5,14 @@ namespace PrviImenik
     class Imenici
     {
 
-        // TODO:020 Pokrenuti program i provjeriti ispise poziva donjih metoda.
+
         static void Main(string[] args)
         {
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
 
-            // TODO:021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
-            MojaKlasa mk2 = new MojaKlasa();
+           
+            DrugiImenik.MojaKlasa mk2 = new DrugiImenik.MojaKlasa();
             mk2.Metoda2();
 
             Console.WriteLine("GOTOVO!!!");
