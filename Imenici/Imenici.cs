31f924cb35e37baf6fd2869fc3bc80ca64cc8550
Senad Mandžić <semandzi@vsite.hr>
2019-10-24 20:03:56 +0200

    Drugi namespaace

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -11,8 +11,7 @@ namespace PrviImenik
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
 
-            // TODO:021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
-            MojaKlasa mk2 = new MojaKlasa();
+            MojaKlasa mk2 = new DrugiImenik.MojaKlasa();
             mk2.Metoda2();
 
             Console.WriteLine("GOTOVO!!!");
