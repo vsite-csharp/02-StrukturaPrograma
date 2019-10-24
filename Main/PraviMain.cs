c6ab72552cae6e41cc882bca098cf587760af515
Auro Mazzi <auro.mazzi@gmail.com>
2019-10-24 19:48:58 +0200

    Remove duplicate Main method

--- Main/PraviMain.cs
+++ Main/PraviMain.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 
-// TODO:000 Napraviti build projekta i pogledati koju pogrešku prevoditelj javlja. Usporediti metode Main u klasama PraviMain i JošJedanMain.
-
-// TODO:001 U postavkama projekta staviti da je klasa PraviMain ulazna (Startup object) te pokrenuti novi build.
-
 namespace Vsite.CSharp.StrukturaPrograma
 {
     class PraviMain
@@ -11,13 +7,13 @@ namespace Vsite.CSharp.StrukturaPrograma
         private static void Main(string[] args)
         {
             Console.WriteLine("Krećem iz PraviMain.Main(string[] args)");
+
         }
 
-        // TODO:002 Zakomentirati ili preimenovati donju metodu i napraviti build projekta te ga pokrenuti.
-        static void Main()
-        {
-            Console.WriteLine("Krećem iz PraviMain.Main()");
-        }
+        //static void Main()
+        //{
+        //    Console.WriteLine("Krećem iz PraviMain.Main()");
+        //}
 
         public static void main()
         {
