6cee270fafa37cc3b416900666eeaaff172112ef
Fran Kalinger <frkaling@vsite.hr>
2019-10-24 20:03:41 +0200

    Resolved Namespaces

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -12,7 +12,7 @@ namespace PrviImenik
             mk1.Metoda1();
 
             // TODO:021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
-            MojaKlasa mk2 = new MojaKlasa();
+            DrugiImenik.MojaKlasa mk2 = new DrugiImenik.MojaKlasa();
             mk2.Metoda2();
 
             Console.WriteLine("GOTOVO!!!");
