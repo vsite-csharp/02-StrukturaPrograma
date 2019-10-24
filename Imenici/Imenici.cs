19c6b0b3a4c42d3fa4780b173f86ebf1e3934ae2
Sven Gotal <svgotal@vsite.hr>
2019-10-24 20:05:17 +0200

    Resolve namespaces

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -5,15 +5,16 @@ namespace PrviImenik
     class Imenici
     {
 
-        // TODO:020 Pokrenuti program i provjeriti ispise poziva donjih metoda.
         static void Main(string[] args)
         {
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
 
-            // TODO:021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
             MojaKlasa mk2 = new MojaKlasa();
             mk2.Metoda2();
+
+            DrugiImenik.MojaKlasa mk_1 = new DrugiImenik.MojaKlasa();
+            mk_1.Metoda2();
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
