0a93e6c353635cca23ad3f7e279d56af3b591ffc
tleva <tleva@DESKTOP-J8PKM7S>
2020-01-14 20:12:36 +0100

    Resolve first 2

--- DokumentacijskiKomentari/DokumentacijskiKomentari.cs
+++ DokumentacijskiKomentari/DokumentacijskiKomentari.cs
@@ -28,6 +28,18 @@ namespace Vsite.CSharp.StrukturaPrograma
         }
 
         // TODO:010 Dodati dokumentacijski komentar za donju metodu i provjeriti pojavljuje li se opis u oblačiću na mjestu poziva metode u metodi Main.
+        /// <summary>
+        ///   Izračunava duljine dvaju rijeci
+        /// </summary>
+        /// <param name="prva">
+        ///  prva rijec
+        /// </param>
+        /// <param name="druga">
+        ///  druga rijec
+        /// </param>
+        /// <returns>
+        ///   zbroj duljina rijeci tipa <c>int</c>.
+        /// </returns>
         static int DuljinaDvijeRiječi(string prva, string druga)
         {
             return (prva + druga).Length;
