914100adcf224b1f0d863d2eca700ee66cfd8f5b
tleva <tleva@DESKTOP-J8PKM7S>
2020-01-14 20:16:14 +0100

    Resolve identifikatori

--- Identifikatori/Identifikatori.cs
+++ Identifikatori/Identifikatori.cs
@@ -9,20 +9,21 @@ namespace Vsite.CSharp.StrukturaPrograma
     {
         static void Main(string[] args)
         {
+            string String;
             // TODO:040 Dodati varijablu s imenom "string". Provjeriti javlja li prevoditelj pogrešku za ime varijable. Ako javlja, napraviti potrebnu promjenu da se ime može koristiti. U protivnom ispisati vrijednost varijable.
 
             // TODO:041 dodati varijablu s imenom "get". Ponoviti postupke iz prethodnog TODO.
-
+            string get;
             // TODO:042 Dodati varijablu s imenom "Main". Ponoviti postupke iz prethodnog TODO.
-
+            string Main;
             // TODO:043 Dodati varijablu sa hrvatskim dijakritičkim znakovima "četrdesetšest". Ponoviti postupke iz prethodnog TODO.
-
+            string četrdesetšest;
             // TODO:044 Dodati istoimenu varijablu "četrdesetšest" i u njoj slovo 'š' zamijeniti njegovim Unicode kodom \u0161 i provjeriti prihvaća li prevoditelj takav oblik imena.
 
             // TODO:045 Dodati cjelobrojnu varijablu s imenom "бројШест" te provjeriti prihvaća li prevoditelj to ime.
-
+            string бројШест;
             // TODO:046 Dodati varijablu "你好" tipa string te provjeriti prihvaća li prevoditelj to ime.
-
+            string 你好;
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
         }
