f9b3498863243b093ed4543d04b7b92d1988ae78
Ivan Čulina <72503859+ivculina@users.noreply.github.com>
2020-10-22 21:07:26 +0200

    Namespace settings

---
+++ Postavkeimenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mojaklasa1 mk1 = new Mojaklasa1();
+            Class2 mk2 = new Class2();
+        }
+    }
+}
