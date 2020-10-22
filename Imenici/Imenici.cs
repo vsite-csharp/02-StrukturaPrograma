3195bbd5993e7e7fa1ec6b3de1bc7f20c37fa2f4
ivana andelic <54909467+ivana474@users.noreply.github.com>
2020-10-22 20:58:23 +0200

    resolve namespaces

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -5,14 +5,13 @@ namespace Vsite.CSharp.PrviImenik
     class Imenici
     {
 
-        // TODO:020 Pokrenuti program i provjeriti ispise poziva donjih metoda.
         static void Main(string[] args)
         {
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
 
             // TODO:021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika Vsite.CSharp.DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
-            MojaKlasa mk2 = new MojaKlasa();
+            Vsite.CSharp.DrugiImenik.MojaKlasa mk2 = new Vsite.CSharp.DrugiImenik.MojaKlasa();
             mk2.Metoda2();
 
             Console.WriteLine("GOTOVO!!!");
