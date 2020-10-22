711e3c3b8ae40b1e0b4187e46a84a31bfc39436a
Klara Blašković <klblasko@vsite.hr>
2020-10-22 21:06:56 +0200

    Resolve namespace setting

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa1 mk1 = new MojaKlasa1();
+            MojaKlasa2 mk2 = new MojaKlasa2();
+        }
+    }
+}
