c45f994e632714f0bcf0c3f296ff4bde0626e78f
Mateo Jadrušić <majadrus@vsite.hr>
2020-10-22 21:07:57 +0200

    MojaKlasa

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa1 = new MojaKlasa2();
+            MojaKlasa2 = new MojaKlasa1();
+        }
+    }
+}
