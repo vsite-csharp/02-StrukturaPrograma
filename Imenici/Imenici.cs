a6fbbff690a9be538a5f7639b76d9ba0be57ff80
Mateo Jadrušić <majadrus@vsite.hr>
2020-10-22 20:58:34 +0200

    Resolve namespace

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -5,14 +5,12 @@ namespace Vsite.CSharp.PrviImenik
     class Imenici
     {
 
-        // TODO:020 Pokrenuti program i provjeriti ispise poziva donjih metoda.
         static void Main(string[] args)
         {
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
 
-            // TODO:021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika Vsite.CSharp.DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
-            MojaKlasa mk2 = new MojaKlasa();
+            Vsite.CSharp.DrugiImenikMojaKlasa mk2 = new MojaKlasa();
             mk2.Metoda2();
 
             Console.WriteLine("GOTOVO!!!");
