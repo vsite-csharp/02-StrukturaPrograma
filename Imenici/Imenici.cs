3040f4d61f4760d552a8a269cdd7160270c6f78f
Zdravko Pašalić <72557820+zdpasali@users.noreply.github.com>
2020-10-22 21:18:03 +0200

    Indetificators

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -10,15 +10,12 @@ namespace Vsite.CSharp.PrviImenik
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
 
-            Vsite.CSharp.DrugiImenik.MojaKlasa mk2 = new Vsite.CSharp.PrviImenik.MojaKlasa();
+            Vsite.CSharp.DrugiImenik.MojaKlasa mk2 = new Vsite.CSharp.DrugiImenik.MojaKlasa();
             mk2.Metoda2();
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
         }
 
-        // TODO:030 U kazalu "03 Promjena postavki imenika" kreirati projekt za konzolnu aplikaciju s imenom PostavkeImenika.
-        // TODO:031 U postavkama projekta promijeniti podrazumijevani imenik u Vsite.CSharp.Imenici te ispraviti imenik u automatski generiranom kodu
-        // TODO:032 Definirati dvije nove klase: MojaKlasa1 i MojaKlasa2 te instancirati objekte tih klasa u metodi Program.Main
     }
 }
---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa1 ak1 = new MojaKlasa1();
+            MojaKlasa2 ak2 = new MojaKlasa2();
+        }
+    }
+}
