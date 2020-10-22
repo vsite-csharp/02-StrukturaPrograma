360a444d72c3289e53ae5e470c1b961681eff065
Ivan <ivzivkovic@vsite.hr>
2020-10-22 21:07:13 +0200

    Resolve namespace setting

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa1 mk1 = new MojaKlasa1();
+            MojaKlasa2 mk2 = new MojaKlasa2();        }
+    }
+}
