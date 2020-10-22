ee72f591c4b1c8302fbcd87bc4ddf11e377f5746
Ivan Čulina <72503859+ivculina@users.noreply.github.com>
2020-10-22 20:46:45 +0200

    Resolve main method

--- Main/PraviMain.cs
+++ Main/PraviMain.cs
@@ -1,19 +1,14 @@
 ﻿using System;
 
-// TODO:000 Napraviti build projekta i pogledati koju pogrešku prevoditelj javlja. Usporediti metode Main u klasama PraviMain i JošJedanMain.
-
-// TODO:001 U postavkama projekta staviti da je klasa PraviMain ulazna (Startup object) te pokrenuti novi build.
-
 namespace Vsite.CSharp.StrukturaPrograma
 {
     class PraviMain
     {
-        private static void Main(string[] args)
+        private static void MyMain(string[] args)
         {
             Console.WriteLine("Krećem iz PraviMain.Main(string[] args)");
         }
 
-        // TODO:002 Zakomentirati ili preimenovati donju metodu i napraviti build projekta te ga pokrenuti.
         static void Main()
         {
             Console.WriteLine("Krećem iz PraviMain.Main()");
