5d6a2e9d1bcad2655b0ca6ee1b2e8ea3906228de
Ante Žunić <anzunic@vsite.hr>
2020-10-22 21:17:46 +0200

    Resolve Identifiers

--- Identifikatori/Identifikatori.cs
+++ Identifikatori/Identifikatori.cs
@@ -9,19 +9,13 @@ namespace Vsite.CSharp.StrukturaPrograma
     {
         static void Main(string[] args)
         {
-            // TODO:040 Dodati varijablu s imenom "string". Provjeriti javlja li prevoditelj pogrešku za ime varijable. Ako javlja, napraviti potrebnu promjenu da se ime može koristiti. U protivnom ispisati vrijednost varijable.
-
-            // TODO:041 dodati varijablu s imenom "get". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:042 Dodati varijablu s imenom "Main". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:043 Dodati varijablu sa hrvatskim dijakritičkim znakovima "četrdesetšest". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:044 Dodati istoimenu varijablu "četrdesetšest" i u njoj slovo 'š' zamijeniti njegovim Unicode kodom \u0161 i provjeriti prihvaća li prevoditelj takav oblik imena.
-
-            // TODO:045 Dodati cjelobrojnu varijablu s imenom "бројШест" te provjeriti prihvaća li prevoditelj to ime.
-
-            // TODO:046 Dodati varijablu "你好" tipa string te provjeriti prihvaća li prevoditelj to ime.
+            int @string = 5;
+            string get = "get";
+            double Main = 3.14;
+            int Četrdesetšest = 46;
+            //int četrdeset\u0161\ = 46;
+            int бројШест = 6;
+            string 你好 = "你好";
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
