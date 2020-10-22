35c6a6b06b37032e119d4bf6cfdbcc6da47a7315
Kristian Petretić <krpetret@vsite.hr>
2020-10-22 21:11:27 +0200

    resolve namespace setting

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -5,13 +5,11 @@ namespace Vsite.CSharp.PrviImenik
     class Imenici
     {
 
-        // TODO:020 Pokrenuti program i provjeriti ispise poziva donjih metoda.
         static void Main(string[] args)
         {
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
 
-            // TODO:021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika Vsite.CSharp.DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
            DrugiImenik. MojaKlasa mk2 = new DrugiImenik.MojaKlasa();
             mk2.Metoda2();
 
@@ -19,8 +17,5 @@ namespace Vsite.CSharp.PrviImenik
             Console.ReadKey(true);
         }
 
-        // TODO:030 U kazalu "03 Promjena postavki imenika" kreirati projekt za konzolnu aplikaciju s imenom PostavkeImenika.
-        // TODO:031 U postavkama projekta promijeniti podrazumijevani imenik u Vsite.CSharp.Imenici te ispraviti imenik u automatski generiranom kodu
-        // TODO:032 Definirati dvije nove klase: MojaKlasa1 i MojaKlasa2 te instancirati objekte tih klasa u metodi Program.Main
     }
 }
---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mojaKlasa1 Mk1 = new mojaklasa();
+            mojaKlasa2 Mk2 = new mojaKlasa2();
+        }
+    }
+}
