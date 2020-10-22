b1d5db9cbfec6c53d0e4f389bd2a8a729facabe3
tin <tin@selmet.com>
2020-10-22 21:17:46 +0200

    Resolved identiviers

--- Identifikatori/Identifikatori.cs
+++ Identifikatori/Identifikatori.cs
@@ -9,21 +9,15 @@ namespace Vsite.CSharp.StrukturaPrograma
     {
         static void Main(string[] args)
         {
-            // TODO:040 Dodati varijablu s imenom "string". Provjeriti javlja li prevoditelj pogrešku za ime varijable. Ako javlja, napraviti potrebnu promjenu da se ime može koristiti. U protivnom ispisati vrijednost varijable.
+            int @string = 2;
+            string get = "get";
+            string četrdesetšest = "s";
+            string cetrdesetisest = "\u0161";
+            string 你好 = "你好";
+            string cirilica = "бројШест";
+            int бројШест = 6;
 
-            // TODO:041 dodati varijablu s imenom "get". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:042 Dodati varijablu s imenom "Main". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:043 Dodati varijablu sa hrvatskim dijakritičkim znakovima "četrdesetšest". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:044 Dodati istoimenu varijablu "četrdesetšest" i u njoj slovo 'š' zamijeniti njegovim Unicode kodom \u0161 i provjeriti prihvaća li prevoditelj takav oblik imena.
-
-            // TODO:045 Dodati cjelobrojnu varijablu s imenom "бројШест" te provjeriti prihvaća li prevoditelj to ime.
-
-            // TODO:046 Dodati varijablu "你好" tipa string te provjeriti prihvaća li prevoditelj to ime.
-
-            Console.WriteLine("GOTOVO!!!");
+            Console.WriteLine("GOTOVO!!!" + cetrdesetisest + 你好 + бројШест);
             Console.ReadKey(true);
         }
     }
