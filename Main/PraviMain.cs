7fd725c7cf1dc4e8252ffec1ec2b89dea7840902
Zvonimir Balen <zvbalen@vsite.hr>
2020-10-22 20:47:34 +0200

    resolve main metod

--- Main/PraviMain.cs
+++ Main/PraviMain.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 
-// TODO:000 Napraviti build projekta i pogledati koju pogrešku prevoditelj javlja. Usporediti metode Main u klasama PraviMain i JošJedanMain.
 
-// TODO:001 U postavkama projekta staviti da je klasa PraviMain ulazna (Startup object) te pokrenuti novi build.
 
 namespace Vsite.CSharp.StrukturaPrograma
 {
@@ -13,8 +11,7 @@ namespace Vsite.CSharp.StrukturaPrograma
             Console.WriteLine("Krećem iz PraviMain.Main(string[] args)");
         }
 
-        // TODO:002 Zakomentirati ili preimenovati donju metodu i napraviti build projekta te ga pokrenuti.
-        static void Main()
+        
         {
             Console.WriteLine("Krećem iz PraviMain.Main()");
         }
