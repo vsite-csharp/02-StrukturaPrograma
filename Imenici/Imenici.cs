2f4dbaff3ba021f6afe385e254e4de8b6f30bc80
ivana andelic <54909467+ivana474@users.noreply.github.com>
2020-10-22 21:07:13 +0200

    resolve namespace setting

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -10,7 +10,6 @@ namespace Vsite.CSharp.PrviImenik
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
 
-            // TODO:021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika Vsite.CSharp.DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
             Vsite.CSharp.DrugiImenik.MojaKlasa mk2 = new Vsite.CSharp.DrugiImenik.MojaKlasa();
             mk2.Metoda2();
 
@@ -18,8 +17,5 @@ namespace Vsite.CSharp.PrviImenik
             Console.ReadKey(true);
         }
 
-        // TODO:030 U kazalu "03 Promjena postavki imenika" kreirati projekt za konzolnu aplikaciju s imenom PostavkeImenika.
-        // TODO:031 U postavkama projekta promijeniti podrazumijevani imenik u Vsite.CSharp.Imenici te ispraviti imenik u automatski generiranom kodu
-        // TODO:032 Definirati dvije nove klase: MojaKlasa1 i MojaKlasa2 te instancirati objekte tih klasa u metodi Program.Main
     }
 }
