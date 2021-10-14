da2bd9fc582a35f683e3a7ec91d5a176df10fadb
Ivan <ivzivkovic@vsite.hr>
2021-10-14 20:53:19 +0200

    Resolve multiple Main methods

--- Main/PraviMain.cs
+++ Main/PraviMain.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-// TODO:000 Napraviti build projekta i pogledati koju pogrešku prevoditelj javlja. Usporediti metode Main u klasama PraviMain i JošJedanMain.
+// 000 Napraviti build projekta i pogledati koju pogrešku prevoditelj javlja. Usporediti metode Main u klasama PraviMain i JošJedanMain.
 
-// TODO:001 U postavkama projekta staviti da je klasa PraviMain ulazna (Startup object) te pokrenuti novi build.
+// 001 U postavkama projekta staviti da je klasa PraviMain ulazna (Startup object) te pokrenuti novi build.
 
 namespace Vsite.CSharp.StrukturaPrograma
 {
@@ -11,13 +11,17 @@ namespace Vsite.CSharp.StrukturaPrograma
         private static void Main(string[] args)
         {
             Console.WriteLine("Krećem iz PraviMain.Main(string[] args)");
-        }
+            Console.WriteLine("args[0]");
+            Console.ReadKey(false);
 
-        // TODO:002 Zakomentirati ili preimenovati donju metodu i napraviti build projekta te ga pokrenuti.
-        static void Main()
-        {
-            Console.WriteLine("Krećem iz PraviMain.Main()");
         }
+        
+
+        // 002 Zakomentirati ili preimenovati donju metodu i napraviti build projekta te ga pokrenuti.
+        //static void Main()
+        //{
+        //    Console.WriteLine("Krećem iz PraviMain.Main()");
+        //}
 
         public static void main()
         {
