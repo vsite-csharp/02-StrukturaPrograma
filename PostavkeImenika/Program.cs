ae66120f548d8164bfbf8bd73c1fcc48afe1f609
tglozinic <tglozinic@dspace.hr>
2021-10-14 21:12:58 +0200

    Reslove changing default namespace for project

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vsite.CSharp.Imenici;
+
+namespace vsite.CSharp.Imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vsite.CSharp.Imenici.Class1 c = new Class1();
+        }
+    }
+}
