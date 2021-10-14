b73bffbbf0252a54cbb4b2e8180cac36af52f329
Vedran Olujic <VOlujic@dspace.hr>
2021-10-14 20:53:12 +0200

    Prvi commit za 02 struktura programa. Odabran Pravi main za izvršavanje.

--- Main/PraviMain.cs
+++ Main/PraviMain.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-// TODO:000 Napraviti build projekta i pogledati koju pogrešku prevoditelj javlja. Usporediti metode Main u klasama PraviMain i JošJedanMain.
+// 000 Napraviti build projekta i pogledati koju pogrešku prevoditelj javlja. Usporediti metode Main u klasama PraviMain i JošJedanMain.
 
 // TODO:001 U postavkama projekta staviti da je klasa PraviMain ulazna (Startup object) te pokrenuti novi build.
 
@@ -11,14 +11,17 @@ namespace Vsite.CSharp.StrukturaPrograma
         private static void Main(string[] args)
         {
             Console.WriteLine("Krećem iz PraviMain.Main(string[] args)");
-        }
+            Console.WriteLine(args[0]);
 
+            Console.ReadKey(false);
+        }
+        /*
         // TODO:002 Zakomentirati ili preimenovati donju metodu i napraviti build projekta te ga pokrenuti.
         static void Main()
         {
             Console.WriteLine("Krećem iz PraviMain.Main()");
         }
-
+        */
         public static void main()
         {
             Console.WriteLine("main()");
