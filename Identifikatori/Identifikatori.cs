fdb3bf10396309d6d807b397e231a5fc38f263f9
Ivan Sokolović <isokolovic1@gmail.com>
2021-10-14 21:26:13 +0200

    Resolved identificators

--- Identifikatori/Identifikatori.cs
+++ Identifikatori/Identifikatori.cs
@@ -9,19 +9,30 @@ namespace Vsite.CSharp.StrukturaPrograma
     {
         static void Main(string[] args)
         {
-            // TODO:040 Dodati varijablu s imenom "string". Provjeriti javlja li prevoditelj pogrešku za ime varijable. Ako javlja, napraviti potrebnu promjenu da se ime može koristiti. U protivnom ispisati vrijednost varijable.
+            // OK-TODO:040 Dodati varijablu s imenom "string". Provjeriti javlja li prevoditelj pogrešku za ime varijable. Ako javlja, napraviti potrebnu promjenu da se ime može koristiti. U protivnom ispisati vrijednost varijable.
+            int @string = 5;
 
-            // TODO:041 dodati varijablu s imenom "get". Ponoviti postupke iz prethodnog TODO.
+            // OK-TODO:041 dodati varijablu s imenom "get". Ponoviti postupke iz prethodnog TODO.
+            string get = "mojaVarijabla"; /*get nije ključna riječ?*/
 
-            // TODO:042 Dodati varijablu s imenom "Main". Ponoviti postupke iz prethodnog TODO.
+            // OK-TODO:042 Dodati varijablu s imenom "Main". Ponoviti postupke iz prethodnog TODO.
+            string main = "mojMain";
 
-            // TODO:043 Dodati varijablu sa hrvatskim dijakritičkim znakovima "četrdesetšest". Ponoviti postupke iz prethodnog TODO.
+            // OK-TODO:043 Dodati varijablu sa hrvatskim dijakritičkim znakovima "četrdesetšest". Ponoviti postupke iz prethodnog TODO.
+            double četrdeseptet = 35.2;
 
-            // TODO:044 Dodati istoimenu varijablu "četrdesetšest" i u njoj slovo 'š' zamijeniti njegovim Unicode kodom \u0161 i provjeriti prihvaća li prevoditelj takav oblik imena.
+            // OK-TODO:044 Dodati istoimenu varijablu "četrdesetšest" i u njoj slovo 'š' zamijeniti njegovim Unicode kodom \u0161 i provjeriti prihvaća li prevoditelj takav oblik imena.
+            double četrdeset\u0161est = 35.2;
 
-            // TODO:045 Dodati cjelobrojnu varijablu s imenom "бројШест" te provjeriti prihvaća li prevoditelj to ime.
+            // OK-TODO:045 Dodati cjelobrojnu varijablu s imenom "бројШест" te provjeriti prihvaća li prevoditelj to ime.
+            short бројШест = 1;
 
-            // TODO:046 Dodati varijablu "你好" tipa string te provjeriti prihvaća li prevoditelj to ime.
+            // OK-TODO:046 Dodati varijablu "你好" tipa string te provjeriti prihvaća li prevoditelj to ime.
+            //string 你好 = "njiHao";
+            string 你好 = "你好";
+
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine(你好);
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
