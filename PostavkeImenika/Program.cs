e71794fe1b41e41932322ba412fa4e7bd0472702
Ivana Rakarić <ivrakari@vsite.hr>
2021-10-14 21:13:22 +0200

    Imenici

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Vsite.CSharp.Imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+
+//using Vsite.CSharp.Imenici
