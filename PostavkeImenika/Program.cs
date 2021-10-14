93ba556e2f0ae0d65e40a440e5c1ef3467200e48
Ognjen Adamović <ognjen.adamovic@billy.hr>
2021-10-14 21:11:28 +0200

    Postavke Imenika

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            Class2 c2 = new Class2();
+        }
+    }
+}
