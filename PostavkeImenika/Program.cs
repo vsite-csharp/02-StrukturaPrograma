6da2e794f349dd67361de92e3b7b3b1a314dee54
Danijel Krpes <dakrpes@vsite.hr>
2021-10-14 19:49:44 +0200

    Resolve namespace

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa1 mk1 = new MojaKlasa1();
+            MojaKlasa2 mk2 = new MojaKlasa2();
+        }
+        public class MojaKlasa1
+        {
+
+        }
+        
+        public class MojaKlasa2
+        {
+
+        }
+    }
+}
