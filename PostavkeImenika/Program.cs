a753d7a1919b1182febd58c5cec978142d627461
Luka Gregorić <endo145@gmail.com>
2021-10-14 21:35:30 +0200

    Resolve missing semicolon

--- PostavkeImenika/Program.cs
+++ PostavkeImenika/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Vsite.CSharp.Imenici
+using Vsite.CSharp.Imenici;
 
 namespace PostavkeImenika
 {
