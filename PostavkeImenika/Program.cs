9cb819c1c010e5290a175524c26cdc72195afbd5
Kata Pogacic <KPogacic@dspace.hr>
2021-10-14 21:12:57 +0200

    Resolve changing default namespace for project

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Vsite.CSharp.Imenici
+
+namespace PostavkeImenika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vsite.CSharp.Imenici.Class1 c = new Vsite.CSharp.Imenici.Class1();
+        }
+    }
+}
