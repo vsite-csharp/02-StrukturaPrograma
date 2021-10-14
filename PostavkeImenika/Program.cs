e81673caad1d58000788b6aa925b95b4d33e49a1
Vedran Olujic <VOlujic@dspace.hr>
2021-10-14 21:29:10 +0200

    02 predavanje

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.CSharp.Imenici;
+
+namespace Vsite.CSharp.Imenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new class1();
+        }
+    }
+}
