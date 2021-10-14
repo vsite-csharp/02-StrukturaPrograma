f86574f9dabc4dd36e17fb7c18a3ad62ec3cec5a
Korisnik <Korisnik@DESKTOP-S2GEHQG>
2021-10-14 21:13:10 +0200

    Resolve changing default namespace

---
+++ Postavkeimenika/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vsite.CSharp.Imenici;
+
+namespace Postavkeimenika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class();
+        }
+    }
+}
