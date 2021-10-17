2514dcd116d40f1c39be07e098647dd7329c1b95
KaTa1112 <katapogacic1@gmail.com>
2021-10-17 18:05:42 +0200

    HomeWork

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -5,15 +5,22 @@ namespace Vsite.CSharp.PrviImenik
     class Imenici
     {
 
-        // TODO:020 Pokrenuti program i provjeriti ispise poziva donjih metoda.
+       //020 Pokrenuti program i provjeriti ispise poziva donjih metoda.
+       /*Ispis: Vsite.CSharp.PrviImenik.MojaKlasa.Metoda1()
+                Vsite.CSharp.PrviImenik.MojaKlasa.Metoda2()
+                GOTOVO!!!*/
         static void Main(string[] args)
         {
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
 
-            // TODO:021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika Vsite.CSharp.DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
-            MojaKlasa mk2 = new MojaKlasa();
+            // 021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika Vsite.CSharp.DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
+            Vsite.CSharp.DrugiImenik.MojaKlasa mk2 = new Vsite.CSharp.DrugiImenik.MojaKlasa();
             mk2.Metoda2();
+
+            /*Vsite.CSharp.PrviImenik.MojaKlasa.Metoda1()
+              Vsite.CSharp.DrugiImenik.MojaKlasa.Metoda2()
+              GOTOVO!!!*/
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
