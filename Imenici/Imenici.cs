aef11e2acb1f3ccd3d95ac50d9812100af34e07e
Leon Pleslic <LPleslic@dspace.hr>
2021-10-20 09:42:03 +0200

    DZ Imenici

--- Imenici/Imenici.cs
+++ Imenici/Imenici.cs
@@ -11,8 +11,7 @@ namespace Vsite.CSharp.PrviImenik
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
 
-            // TODO:021 Promijeniti donji poziv tako da se poziva klasa MojaKlasa iz imenika Vsite.CSharp.DrugiImenik. Napraviti build, pokrenuti program i provjeriti ispise.
-            MojaKlasa mk2 = new MojaKlasa();
+            Vsite.CSharp.DrugiImenik.MojaKlasa mk2 = new Vsite.CSharp.DrugiImenik.MojaKlasa();
             mk2.Metoda2();
 
             Console.WriteLine("GOTOVO!!!");
