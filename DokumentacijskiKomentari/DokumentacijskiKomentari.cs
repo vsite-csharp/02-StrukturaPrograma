b963b03dd0b9bd959ff5b32ea9107dd8c378a5c9
Jacksha <jkvljvc@yahoo.com>
2022-10-20 21:01:34 +0200

    add documentation comment

--- DokumentacijskiKomentari/DokumentacijskiKomentari.cs
+++ DokumentacijskiKomentari/DokumentacijskiKomentari.cs
@@ -4,7 +4,7 @@ namespace Vsite.CSharp.StrukturaPrograma
 {
     class DokumentacijskiKomentari
     {
-        // TODO:010 Postaviti strelicu (kurzor) iznad donjeg poziva metode Kvadrat i provjeriti što se prikazuje u oblačiću (eventualno napraviti build prije toga).
+        // Postaviti strelicu (kurzor) iznad donjeg poziva metode Kvadrat i provjeriti što se prikazuje u oblačiću (eventualno napraviti build prije toga).
         static void Main(string[] args)
         {
             Console.WriteLine(Kvadrat(5));
@@ -25,7 +25,21 @@ namespace Vsite.CSharp.StrukturaPrograma
             return broj * broj;
         }
 
-        // TODO:011 Dodati dokumentacijski komentar za donju metodu i provjeriti pojavljuje li se opis u oblačiću na mjestu poziva metode u metodi Main.
+        // Dodati dokumentacijski komentar za donju metodu i provjeriti pojavljuje li se opis u oblačiću na mjestu poziva metode u metodi Main.
+
+        /// <summary>
+        ///     Izracunava duljinu dviju spojenih rijeci
+        /// </summary>
+        /// <param name="prva">
+        ///     prva riječ
+        /// </param>
+        /// <param name="druga">
+        ///     druga riječ
+        /// </param>
+        /// <returns>
+        ///     vraća ukupnu duljinu riječi
+        /// </returns>
+
         static int DuljinaDvijeRiječi(string prva, string druga)
         {
             return prva.Length + druga.Length;
