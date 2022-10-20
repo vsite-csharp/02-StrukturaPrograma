3f8e2f9a9ad54a27d1ec0cc247e62c1794a4f41e
Nedim Omerović <neomerov@vsite.hr>
2022-10-20 21:27:01 +0200

    identificators

--- Identifikatori/Identifikatori.cs
+++ Identifikatori/Identifikatori.cs
@@ -9,20 +9,21 @@ namespace Vsite.CSharp.StrukturaPrograma
     {
         static void Main(string[] args)
         {
-            // TODO:040 Dodati varijablu s imenom "string". Provjeriti javlja li prevoditelj pogrešku za ime varijable. Ako javlja, napraviti potrebnu promjenu da se ime može koristiti. U protivnom ispisati vrijednost varijable.
+            // Dodati varijablu s imenom "string". Provjeriti javlja li prevoditelj pogrešku za ime varijable. Ako javlja, napraviti potrebnu promjenu da se ime može koristiti. U protivnom ispisati vrijednost varijable.
+            int @string = 5;
+            // dodati varijablu s imenom "get". Ponoviti postupke iz prethodnog TODO.
+            string get = "pero";
+            // Dodati varijablu s imenom "Main". Ponoviti postupke iz prethodnog TODO.
+            double Main = 3.14
+            //  Dodati varijablu sa hrvatskim dijakritičkim znakovima "četrdesetšest". Ponoviti postupke iz prethodnog TODO.
+            int četrdesetšest = 46;
+            // Dodati istoimenu varijablu "četrdesetšest" i u njoj slovo 'š' zamijeniti njegovim Unicode kodom \u0161 i provjeriti prihvaća li prevoditelj takav oblik imena.
 
-            // TODO:041 dodati varijablu s imenom "get". Ponoviti postupke iz prethodnog TODO.
 
-            // TODO:042 Dodati varijablu s imenom "Main". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:043 Dodati varijablu sa hrvatskim dijakritičkim znakovima "četrdesetšest". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:044 Dodati istoimenu varijablu "četrdesetšest" i u njoj slovo 'š' zamijeniti njegovim Unicode kodom \u0161 i provjeriti prihvaća li prevoditelj takav oblik imena.
-
-            // TODO:045 Dodati cjelobrojnu varijablu s imenom "бројШест" te provjeriti prihvaća li prevoditelj to ime.
-
-            // TODO:046 Dodati varijablu "你好" tipa string te provjeriti prihvaća li prevoditelj to ime.
-
+            //  Dodati cjelobrojnu varijablu s imenom "бројШест" te provjeriti prihvaća li prevoditelj to ime.
+            int бројШест = 6;
+            // Dodati varijablu "你好" tipa string te provjeriti prihvaća li prevoditelj to ime.
+            string 你好 = "dobar dan";
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
         }
