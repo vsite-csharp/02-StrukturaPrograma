6742b696a233b64f6a6e4ea4d2d120439fe6bb47
Jacksha <jkvljvc@yahoo.com>
2022-10-20 21:16:57 +0200

    resolve namespace deffinition

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Vsite.Charp.StrukturaPrograma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
