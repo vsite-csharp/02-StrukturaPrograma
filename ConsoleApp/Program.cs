32f125ed8c76b5694a6c294ac0fd6923fb726f25
Giancarlo <giancarlo.bedek@hotmail.com>
2022-10-20 21:17:12 +0200

    Resolve namespace definition

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Vsite.Csharp.StrukturaPrograma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
