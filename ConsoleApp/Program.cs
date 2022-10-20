3a2a4907d450e62208358dfe541d8c078a7194bb
Marko Bohač <mabohac@vsite.hr>
2022-10-20 21:17:01 +0200

    Resolve namespace definition

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Vsite.CSharp.StrukturaPrograma {  // ako vidis resources u redu je
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
