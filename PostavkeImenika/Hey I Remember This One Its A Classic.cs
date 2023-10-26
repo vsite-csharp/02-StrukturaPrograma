ab18d8d4eb01cb8e9667b330b1c20de09f987eb9
Mihael Krpes <mikrpes@vsite.hr>
2023-10-26 19:58:37 +0200

    Resolve namespace resolution and default namespace definition

---
+++ PostavkeImenika/Hey I Remember This One Its A Classic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Imenici
+{
+    internal class Hey_I_Remember_This_One_Its_A_Classic
+    {
+    }
+}
