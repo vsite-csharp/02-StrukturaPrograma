ec0f117e517bb2819440c632b5419fc933479deb
Kristian Mesić <kristian.mesic303@gmail.com>
2023-10-26 19:58:34 +0200

    - Made new Project - Changed Default Namespace for project - Created two new Classes and called them inside Program.cs

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,16 @@
+﻿using PostavkeImenika;
+
+namespace Vsite.CSharp.Imenici
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var mk = new MojaKlasa1();
+
+            var mk2 = new MojaKlasa2();
+        }
+    }
+}
