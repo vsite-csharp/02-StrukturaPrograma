3da30dd3320fba53bfdfceb4cb28382cdf9d9c2f
Kristian Mesić <kristian.mesic303@gmail.com>
2023-10-26 20:10:00 +0200

    - Resolve identifiers

--- Identifikatori/Identifikatori.cs
+++ Identifikatori/Identifikatori.cs
@@ -4,20 +4,22 @@
     {
         static void Main(string[] args)
         {
-            // TODO:040 Dodati varijablu s imenom "string". Provjeriti javlja li prevoditelj pogrešku za ime varijable. Ako javlja, napraviti potrebnu promjenu da se ime može koristiti. U protivnom ispisati vrijednost varijable.
+            // :040 Dodati varijablu s imenom "string". Provjeriti javlja li prevoditelj pogrešku za ime varijable. Ako javlja, napraviti potrebnu promjenu da se ime može koristiti. U protivnom ispisati vrijednost varijable.
+            int @string = 6;
 
-            // TODO:041 dodati varijablu s imenom "get". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:042 Dodati varijablu s imenom "Main". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:043 Dodati varijablu sa hrvatskim dijakritičkim znakovima "četrdesetšest". Ponoviti postupke iz prethodnog TODO.
-
-            // TODO:044 Dodati istoimenu varijablu "četrdesetšest" i u njoj slovo 'š' zamijeniti njegovim Unicode kodom \u0161 i provjeriti prihvaća li prevoditelj takav oblik imena.
-
-            // TODO:045 Dodati cjelobrojnu varijablu s imenom "бројШест" te provjeriti prihvaća li prevoditelj to ime.
-
-            // TODO:046 Dodati varijablu "你好" tipa string te provjeriti prihvaća li prevoditelj to ime.
-
+            // :041 dodati varijablu s imenom "get". Ponoviti postupke iz prethodnog TODO.
+            string get = "Pero";
+            // :042 Dodati varijablu s imenom "Main". Ponoviti postupke iz prethodnog TODO.
+            double Main = 3.14;
+            // :043 Dodati varijablu sa hrvatskim dijakritičkim znakovima "četrdesetšest". Ponoviti postupke iz prethodnog TODO.
+            int četrdesetšest = 46;
+            // :044 Dodati istoimenu varijablu "četrdesetšest" i u njoj slovo 'š' zamijeniti njegovim Unicode kodom \u0161 i provjeriti prihvaća li prevoditelj takav oblik imena.
+            int _četrdeset\u0161est = 2;
+            // :045 Dodati cjelobrojnu varijablu s imenom "бројШест" te provjeriti prihvaća li prevoditelj to ime.
+            int бројШест = 3*2;
+            // :046 Dodati varijablu "你好" tipa string te provjeriti prihvaća li prevoditelj to ime.
+            string 你好 = "Kinezi";
+            Console.WriteLine("你好"); // cw out = ??
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
         }
