b7403af92b69a565844ce73a77d21413cc0f121c
Sonja Perković <soperkov@vsite.hr>
2023-10-26 19:58:51 +0200

    Resolve namespace resolution and default namespace definition

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,13 @@
+﻿using PostavkeImenika;
+
+namespace Vsite.Csharp.Imenici
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MojaKlasa mk = new MojaKlasa();
+        }
+    }
+}
