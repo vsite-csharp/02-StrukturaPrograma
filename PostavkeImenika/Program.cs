2a41000b16dac7848cabb0dc57fd082a250511d8
Kristian Bolarić <krbolaric@vsite.hr>
2023-10-26 19:59:07 +0200

    Resolve namespace resolution and default namespace definition

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,12 @@
+﻿using PostavkeImenika;
+namespace Vsite.CSharp.Imenici
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var mk = new MojaKlasa();
+        }
+    }
+}
